951d560d3bbec0a86efc05877822afc6c2f1e386
Raphael Varonos <r.varonos@agneta.io>
2018-02-22 21:46:30 +0100

    Remove models from detached project.

--- services/model-config.js
+++ services/model-config.js
@@ -3,34 +3,6 @@ module.exports = {
     dataSource: 'db',
     public: true
   },
-  Role_Member: {
-    dataSource: 'db',
-    public: true
-  },
-  Member_Order: {
-    dataSource: 'db',
-    public: false
-  },
-  Member_Topup: {
-    dataSource: 'db',
-    public: false
-  },
-  Order_Item: {
-    dataSource: false,
-    public: false
-  },
-  Order_Event: {
-    dataSource: 'db',
-    public: false
-  },
-  Order_Treasure: {
-    dataSource: 'db',
-    public: false
-  },
-  Order_Garden: {
-    dataSource: 'db',
-    public: false
-  },
   Payment_Receipt: {
     dataSource: 'db',
     public: false
